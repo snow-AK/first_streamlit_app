43d939e8e048ac6385380b908b4986ce49f144af
Akash <122478814+snow-AK@users.noreply.github.com>
2023-01-12 11:20:20 +0530

    Added fruits to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,14 @@
 import streamlit
+import pandas
 
 streamlit.title("My parents new healthy diner")
 streamlit.header("Breakfast Menu")
 streamlit.text(" 🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text(" 🥗 Kale, Spinach & Rocket smoothie")
-streamlit.text(" 🐔 Hard-Boiled, free-reange egg")
+streamlit.text(" 🐔 Hard-Boiled, free-range egg")
 streamlit.text(" 🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
