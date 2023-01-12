94f7ad88f05afd785fdb53ceb4eeb7bcb699a29e
Akash <122478814+snow-AK@users.noreply.github.com>
2023-01-12 11:01:26 +0530

    Added fruit menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.text(" 🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text(" 🥗 Kale, Spinach & Rocket smoothie")
 streamlit.text(" 🐔 Hard-Boiled, free-reange egg")
 streamlit.text(" 🥑🍞 Avocado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
