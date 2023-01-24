683792db5735ce75b4b9ae7cec1f0338d632228c
Akash <122478814+snow-AK@users.noreply.github.com>
2023-01-24 19:50:36 +0530

    Normalize fruityvice JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.text(fruityvice_response.json())
+
+# normalize JSON
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display it as a table
+streamlit.dataframe(fruityvice_normalized)
+
 
